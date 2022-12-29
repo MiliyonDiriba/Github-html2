179f3b66e55ff972613ebe82cc8702436d39cbbe
MiliyonDiriba <120785523+MiliyonDiriba@users.noreply.github.com>
2022-12-29 11:25:49 -0800

    Script file is modified

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ function onefunction() {
     document.getElementById("well").style.fontSize ="25px";
     document.getElementById("well").style.backgroundColor ="blue"
     }
+    function fun() {
+        document.getElementById("hello").innerHTML="welcome";
+        document.getElementById("hello").style.color="Green";
+        
+
+    }
